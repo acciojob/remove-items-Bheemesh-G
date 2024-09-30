878ce6f034165385aae326fcd577013ebce3809c
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-30 19:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const button = document.getElementsByTagName('input');
+
+button[0].addEventListener('click',(event)=>{
+
+	const colorSelect = document.getElementById("colorSelect");
+
+	const selectedIndex = colorSelect.selectedIndex;
+
+	if(selectedIndex!==-1)
+	{
+		colorSelect.remove(selectedIndex);
+	}
+});
